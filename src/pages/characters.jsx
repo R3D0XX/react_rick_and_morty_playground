476dd5de310ea86118d1629ae2ff9e299687c6f7
R3D0XX <dr.doenges@tamsjadi.com>
2023-09-07 14:41:20 +0200

    first_commit

---
+++ src/pages/characters.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+function Characters() {
+
+    const [characters, setCharacters] = useState([]);
+    const [error, setError] = useState(false);
+    const url = ("https://rickandmortyapi.com/api/character/");
+  
+   useEffect(() => {
+        const fetchCharacter = async () => {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                // console.log('data', data)
+
+                if (!data.error) {
+                    setCharacters(data.results);
+                    // console.log('chars', chars);
+                    
+                } else {
+                    setError(true);
+                }
+            } catch (error) {
+                setError(true);
+                console.log('error', error);
+            }
+        };
+
+        fetchCharacter();
+   }, []); 
+    
+    
+    return (
+        <div>
+            <h1>Rick & Morty</h1>
+            {error ? (
+                <p>error</p>
+            ) : (
+                <ul>
+                    {characters.map((character) => (
+                        <li key={character.id}>
+                            <img src={character.image} alt={character.name} />
+                            {character.name}
+                        </li>
+                    
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default Characters;
