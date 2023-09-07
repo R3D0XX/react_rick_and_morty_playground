864fefc30cbc54635545910cef8ab2eb85849d7b
R3D0XX <dr.doenges@tamsjadi.com>
2023-09-07 14:42:39 +0200

    fetching_and_img_working

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import CardDiv from './components/CardDiv';
+import Characters from './pages/characters'
+// import { fetchData } from './components/api'
+function App() {
+ 
+
+
+
+
+  return (
+    <div>
+      <h1></h1>
+      <Characters />
+      <CardDiv/>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App
